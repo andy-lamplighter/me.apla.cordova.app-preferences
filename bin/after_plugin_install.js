118d928b6fd60879effa39bfbfcf1da1ad90b228
Andy Minkstein <andy@lamplightergames.com>
2024-03-30 08:32:32 -0400

    fix after plugin

--- bin/after_plugin_install.js
+++ bin/after_plugin_install.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = function (context) {
-		var Q = require('q'),
-		path = require('path'),
-		fs = require("./lib/filesystem")(Q, req('fs'), path),
-		settings = require("./lib/settings")(fs, path),
-		android = require("./lib/android")(context),
-		ios = require("./lib/ios")(Q, fs, path, req('plist'), require('xcode'));
+  var Q = require('q'),
+  path = require('path'),
+  fs = require("./lib/filesystem")(Q, require('fs'), path),
+  settings = require("./lib/settings")(fs, path),
+  android = require("./lib/android")(context),
+  ios = require("./lib/ios")(Q, fs, path, require('plist'), require('xcode'));
 
 	return settings.get()
 		.then(function (config) {
